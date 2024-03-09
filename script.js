0f5640b1c974e28a2704528523cffd92dabe6a50
Ohjay33 <61249083+Ohjay33@users.noreply.github.com>
2024-03-09 16:17:44 -0600

    commit1

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
+// Function to display the current date
+function displayDate() {
+    // Create a new Date object
+    var currentDate = new Date();
 
-var d = new Date();
-var day = d.getDay();
-var msg = ["Sunday Funday!",
-    "Back to the grind!",
-    "aye It's Taco Tuesday!",
-    "Boom! Wednesday!",
-    "Thirsty Thursday, yo!",
-    "It's Friday! We should be kickin' it!",
-    "Paaarrrtttyyy!"]
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    var dayOfWeek = currentDate.getDay();
+    
+    // Array of messages for each day of the week
+    var messages = [
+        "Happy Sunday! It's a day to relax and recharge.",
+        " Monday! Time to get this Money!!!",
+        "Tuesday! Lets stay focused and productive.",
+        "Wednesday! We half way there! Keep Pushing.",
+        "Happy Thursday! Keep pushing forward, the weekend is near.",
+        "Its Friday! Time to celebrate the end of the week.",
+        "Saturday! Enjoy your weekend and have fun!"
+    ];
 
-$('#' + day).attr('id', 'today');
-$("#message").text(msg[day]);
-console.log("we getting somewhere");
+    // Construct the date string
+    var dateString = "Today is " + currentDate.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+    // Get the message for the current day
+    var message = messages[dayOfWeek];
+
+    // Display the date and message in the HTML elements
+    document.getElementById("currentDate").textContent = dateString;
+    document.getElementById("message").textContent = message;
+}
+
+// // Call the displayDateAndMessage function when the page loads
+// window.onload = displayDate;
+
+
+// Call the displayDateAndMessage function when the page loads
+button.onclick = displayDate;
+
+
+function setBg () {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    console.log("Done! You did it!!!");
+    // color.innerHTML = "#" + randomColor;
+}
 
 
 
 
 
 
-// How to get the day with javascript
 
-/* <p id="datetime"></p> */
 
-// get current date and time
-// var now = new Date();
-// var datetime = now.toLocaleString();
-// var msg = ["Sunday Funday!",
-//     "Back to the grind!",
-//     "aye It's Taco Tuesday!",
-//     "Boom! Wednesday!",
-//     "Thirsty Thursday, yo!",
-//     "It's Friday! We should be kickin' it!",
-//     "Paaarrrtttyyy!"]
 
-// Insert date and time into HTML
-// document.getElementById("date").innerHTML = datetime;
+
+
 
